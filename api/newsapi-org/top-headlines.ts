1c9ee69b88cbdf045337600bff8c47431dc305bc
Nikolai_Iakushchenko <nick22783@gmail.com>
2023-07-13 16:59:03 +0300

    add time tag

--- api/newsapi-org/top-headlines.ts
+++ api/newsapi-org/top-headlines.ts
@@ -1,8 +1,8 @@
-import {API_KEY} from "@/api/newsapi-org/api-key";
+import { API_KEY } from "@/api/newsapi-org/api-key";
 import axios from "axios";
 
 export interface TopHeadlines {
-  source: object;
+  source: { id: string; name: string };
   author: string;
   title: string;
   description: string;
@@ -12,14 +12,14 @@ export interface TopHeadlines {
   content: string;
 }
 
-export const topHeadlinesUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+export const topHeadlinesUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 
 export const getTopHeadlines = async (): Promise<TopHeadlines[]> => {
-  const response = await axios.get(topHeadlinesUrl)
+  const response = await axios.get(topHeadlinesUrl);
 
   if (response.data.status !== "ok") {
     console.log("response", response);
   }
 
-  return response.data.articles
-}
+  return response.data.articles;
+};
--- components/main-section/MainSection.tsx
+++ components/main-section/MainSection.tsx
@@ -4,6 +4,8 @@ import classes from "./MainSection.module.css";
 export default async function MainSection() {
   const articles = await getTopHeadlines();
 
+  console.log("articles[0].source", articles[0].source);
+
   return (
     <section>
       <div>
@@ -11,8 +13,28 @@ export default async function MainSection() {
       </div>
       <ul>
         {articles.map((article) => (
-          <li className={classes.arcticleItem} key={article.url}>
-            {article.title}
+          <li key={article.url}>
+            <div className={classes.article}>
+              <div className={classes.articleSection}>
+                <div>
+                  <img src={article.urlToImage} alt={article.title} />
+                </div>
+                <div className={classes.articleTitle}>
+                  <h2>{article.title}</h2>
+                </div>
+              </div>
+              <div className={classes.articleSection}>
+                <div className={classes.articleDescription}>
+                  {article.description}
+                </div>
+                <time
+                  dateTime={article.publishedAt}
+                  className={classes.articlePublishedAt}
+                >
+                  {article.publishedAt}
+                </time>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
